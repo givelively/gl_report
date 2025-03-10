b43b2a1ddc2b001a817afdd9c012be6e1f53e514
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-03-10 14:51:28 -0400

    base functionality

---
+++ lib/gl_report/base_report.rb
@@ -0,0 +1,78 @@
+module GlReport
+  class BaseReport
+    class << self
+      def model(klass = nil)
+        if klass
+          @model = klass
+        else
+          @model
+        end
+      end
+
+      def column(key, options = {})
+        _columns[key] = options
+      end
+
+      def _columns
+        @_columns ||= {}
+      end
+
+      def where(conditions)
+        FilteredRelation.new(report_relation, self).where(conditions)
+      end
+
+      def report_relation
+        raise Error, "Model is not defined for #{name}" unless _model
+
+        relation = _model.all
+
+        # Apply any joins defined in the column options
+        _columns.each_value do |opts|
+          relation = relation.left_outer_joins(opts[:joins]) if opts[:joins]
+        end
+
+        # Always select the primary id
+        relation = relation.select("#{_model.table_name}.id AS id")
+
+        # Add selects for all columns that need database data
+        used_selects = {}
+        _columns.each do |_key, opts|
+          next unless opts[:select]
+          
+          opts[:select].each do |alias_name, sql_fragment|
+            next if used_selects[alias_name]
+            
+            used_selects[alias_name] = true
+            relation = relation.select("#{sql_fragment} AS #{alias_name}")
+          end
+        end
+
+        relation
+      end
+
+      protected
+
+      def _model
+        @model
+      end
+    end
+
+    attr_reader :scope
+
+    def initialize(scope: nil)
+      @scope = scope
+    end
+
+    def run
+      relation = self.class.report_relation
+      relation = relation.merge(scope) if scope
+      relation.to_a.map { |record| computed_row(record) }
+    end
+
+    def computed_row(record)
+      self.class._columns.transform_values do |column_def|
+        column_def[:value].call(record, self)
+      end
+    end
+  end
+end
---
+++ lib/gl_report/filtered_relation.rb
@@ -0,0 +1,71 @@
+module GlReport
+  class FilteredRelation
+    attr_reader :relation, :report_class, :pending_filters
+
+    def initialize(relation, report_class)
+      @relation = relation
+      @report_class = report_class
+      @pending_filters = {}
+    end
+
+    def where(conditions)
+      new_filters = conditions.dup
+      new_relation = relation
+
+      # Apply SQL filters where possible
+      conditions.each do |column_key, operators|
+        column_def = report_class._columns[column_key]
+        raise Error, "Unknown column: #{column_key}" unless column_def
+
+        strategy = FilterStrategy.new(column_def)
+        
+        if strategy.sql_filterable?
+          operators.each do |operator, value|
+            new_relation = strategy.apply_to_relation(new_relation, operator, value)
+          end
+          new_filters.delete(column_key)
+        end
+      end
+
+      # Store remaining filters for post-processing
+      FilteredRelation.new(new_relation, report_class).tap do |fr|
+        fr.pending_filters.merge!(new_filters)
+      end
+    end
+
+    def to_a
+      results = relation.to_a.map do |record|
+        report_class.new(scope: nil).computed_row(record)
+      end
+
+      return results if pending_filters.empty?
+
+      # Apply any remaining virtual filters
+      results.select do |row|
+        pending_filters.all? do |column_key, operators|
+          column_def = report_class._columns[column_key]
+          strategy = FilterStrategy.new(column_def)
+          
+          operators.all? do |operator, target_value|
+            strategy.matches?(row[column_key], operator, target_value)
+          end
+        end
+      end
+    end
+
+    def method_missing(method, *args, &block)
+      if relation.respond_to?(method)
+        new_relation = relation.send(method, *args, &block)
+        FilteredRelation.new(new_relation, report_class).tap do |fr|
+          fr.pending_filters.merge!(pending_filters)
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method, include_private = false)
+      relation.respond_to?(method) || super
+    end
+  end
+end
